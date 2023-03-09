98dc536c8bbb3492bd0c59ba7227952a2dc3a67c
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-03-09 21:02:02 +0530

    Add files via upload

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int n1 = 0 ; 
+        int n2 = 1 ; 
+        int n3 = 0 ;
+        for ( int i = 0 ; i < n ; i++ )
+        {
+            n3 = n1 + n2 ;
+            n1 = n2 ;
+            n2 = n3 ;
+        }
+        return n3 ;
+    }
+};
