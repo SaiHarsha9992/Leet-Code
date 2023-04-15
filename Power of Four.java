08335bce4d02e2e19dc4fb6fb2b51e5ca96dd733
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-04-15 20:01:03 +0530

    Add files via upload

---
+++ Power of Four.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        for(int i=0;i<31;i++)
+        {
+            if(Math.pow(4,i)==n)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
