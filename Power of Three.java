63b932f5cbaa863f99ce27d7b602c6910a6f39c4
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-04-15 20:01:19 +0530

    Add files via upload

---
+++ Power of Three.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        for(int i=0;i<31;i++)
+        {
+            if(Math.pow(3,i)==n)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
