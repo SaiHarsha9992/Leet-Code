bba31e9c21760ba9b890db59922cf50d97de2952
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-06-10 20:36:59 +0530

    Add files via upload

---
+++ N repeated element in size 2n array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+       for(int i = 0; i < nums.length ; i++)
+       {
+           for(int j = 0; j < nums.length ; j++)
+           {
+               if(i !=j && nums[i]==nums[j] )
+               {
+                  return nums[i];
+               }
+           }
+           
+       }
+       return 0;
+    }
+}
