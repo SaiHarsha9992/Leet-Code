e41aacc75b7dee93c2c56a3c71dc03eb48e699e0
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-08-27 21:28:13 +0530

    Add files via upload

---
+++ FInd the index of the first occurrence in a string.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if(needle in haystack):
+            i = haystack.index(needle)
+            return i
+        else:
+            return -1
