8d80a6fe7d411206ff8785a9df50e0b01a343831
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-01-07 22:08:23 +0530

    Add files via upload

---
+++ Complement of base 10 integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        x=""
+        for i in bin(n)[2:]:
+            if i=="1":
+                x+="0"
+            else:
+                x+="1"
+        return int(x,2)
+        
