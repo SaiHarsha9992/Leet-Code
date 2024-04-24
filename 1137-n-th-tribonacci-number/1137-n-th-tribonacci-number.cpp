10d60b81b9056f80fbd84add0817895c0131c906
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-04-24 09:05:44 +0530

    Time: 2 ms (50.95%), Space: 7.1 MB (53.59%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n == 0)
+        {
+            return 0;
+        }
+        int a = 0 , b = 1 , c = 1;
+        int d = a+b+c;
+        for(int i = 3 ; i <= n ; i++)
+        {
+            d = a+b+c;
+            a = b ;
+            b = c ;
+            c = d ;
+        }
+        return c;
+    }
+};
