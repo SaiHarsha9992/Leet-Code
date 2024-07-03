472e55762520b399f21057d00495a91ddc1209da
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-07-03 19:53:12 +0530

    Time: 277 ms (62.27%) | Memory: 27.9 MB (19.81%) - LeetSync

---
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        if len(nums) <= 4:
+            return 0
+        nums.sort()
+        k = len(nums) - 3
+        ans = nums[-1] - nums[0]
+        for i in range(k - 1, len(nums)):
+            ans = min(ans, nums[i] - nums[i - k + 1])
+        return ans  
+        
