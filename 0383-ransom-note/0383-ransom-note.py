0c9c342a677ca3f2f556d049ee8512914287508d
Kedarinadh Sai Harsha Gadu <gaduharsha72@gmail.com>
2025-08-25 21:02:59 +0530

    Time: 19 ms (57.11%), Space: 17.7 MB (99.71%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        maga_hash = {}
+
+        for c in magazine:
+            maga_hash[c] = 1 + maga_hash.get(c, 0)
+
+        for c in ransomNote:
+            if c not in maga_hash or maga_hash[c] <= 0:
+                return False
+            maga_hash[c] -= 1
+        
+        return True
